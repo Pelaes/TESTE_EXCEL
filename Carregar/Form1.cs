d1d33fc5bc4d1a439721ac627039f65fa3cf0aaf
Pelaes <celo.pelaes@hotmail.com>
2018-03-12 20:15:58 -0300

    versão funcionando

--- Carregar/Form1.cs
+++ Carregar/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.OleDb;
+using System.IO;
+//using Microsoft.Office.Tools.Excel;//fazer referencia no assembly
+//using Microsoft.Office.Interop.Excel;//fazer referencia no assembly
 
 namespace Carregar
 {
@@ -20,29 +23,53 @@ namespace Carregar
 
         private void button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog arquivo = new OpenFileDialog();
-            arquivo.Filter = "Arquivos Excel (*.xls; *.xlsx) | *.xls; *.xlsx"; //filtro para excel
-            arquivo.Multiselect = false; //permite ou não seleção multipla
-            arquivo.Title = "Selecione o arquivo"; //titulo da caixa de dialogo
-                                                   
-             //if (arquivo.ShowDialog() == DialogResult.OK)
-             
-             
-            
-             
-             
-            string nomesemextensao;
-            if (arquivo.ShowDialog() == DialogResult.OK)
+            try
             {
-                string caminho = arquivo.FileName;
-                nomesemextensao = System.IO.Path.GetFileNameWithoutExtension(arquivo.FileName);
-            
-                string pathconn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + caminho + ";Extended Properties='Excel 12.0;HDR=Yes;IMEX=1;'";
-                OleDbConnection conn = new OleDbConnection(pathconn);
-                OleDbDataAdapter MyDataAdapter = new OleDbDataAdapter("Select * from [" + nomesemextensao + "$]", conn);
-                DataTable dt = new DataTable();
-                MyDataAdapter.Fill(dt);
-                dataGridView1.DataSource = dt;
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();  //cria objeto para buscar arquivo
+                openFileDialog1.Filter = "Arquivos Excel (*.xml; *.xls; *.xlsx; *.xlsm; *.xlsb) |*.xml; *.xls; *.xlsx; *.xlsm; *.xlsb";//cria filtros para arquivos excel
+                openFileDialog1.FilterIndex = 3;
+
+                openFileDialog1.Multiselect = false;        //valor falso não permite seleção multipla de arquivos
+                openFileDialog1.Title = "Escolha um arquivo Microsoft Excel";   //define o nome da caixa de dialogo
+                openFileDialog1.InitialDirectory = @"Desktop"; //define o diretorio inicial
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)//Abrindo um arquivo
+                {
+                    string pathName = openFileDialog1.FileName; //retorna a string com o endereço do arquivo
+                    //string fileName = System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.FileName); //retorna a string com o nome do arquivo sem extensão
+                    DataTable tbContainer = new DataTable(); // cria um objeto datatable
+                    string strConn = string.Empty;
+                    //string sheetName = filename;
+
+                    FileInfo file = new FileInfo(pathName);
+                    if (!file.Exists)
+                    {
+                        throw new Exception("Error, file doesn't exists!");
+                    }
+                    string extension = file.Extension;
+                    switch (extension)
+                    {
+                        case ".xls":
+                            strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + pathName + ";Extended Properties='Excel 8.0;HDR=Yes;IMEX=1;'";
+                            break;
+                        case ".xlsx":
+                            strConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + pathName + ";Extended Properties='Excel 12.0;HDR=Yes;IMEX=1;'";
+                            break;
+                        default:
+                            strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + pathName + ";Extended Properties='Excel 8.0;HDR=Yes;IMEX=1;'";
+                            break;
+                    }
+                    OleDbConnection cnnxls = new OleDbConnection(strConn);
+                    OleDbDataAdapter oda = new OleDbDataAdapter(string.Format("select * from [planilha1$]"), cnnxls);
+                    oda.Fill(tbContainer);
+
+                    dataGridView1.DataSource = tbContainer;
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error!");
             }
         }
     }
